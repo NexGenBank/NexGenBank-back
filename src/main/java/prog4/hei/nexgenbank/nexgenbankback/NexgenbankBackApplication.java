e39ea92c026319b3207f132c2ce3191a091205a3
RSitrakaFL <hei.sitraka@gmail.com>
2024-03-13 09:12:28 +0300

    Initial commit

---
+++ src/main/java/prog4/hei/nexgenbank/nexgenbankback/NexgenbankBackApplication.java
@@ -0,0 +1,13 @@
+package prog4.hei.nexgenbank.nexgenbankback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexgenbankBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexgenbankBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prog4/hei/nexgenbank/nexgenbankback/NexgenbankBackApplicationTests.java
@@ -0,0 +1,13 @@
+package prog4.hei.nexgenbank.nexgenbankback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexgenbankBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
