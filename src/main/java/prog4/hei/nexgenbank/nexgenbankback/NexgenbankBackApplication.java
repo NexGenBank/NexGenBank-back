c16a3fafe3743dd060dfbc2ac9dbadb465e08e61
RSitrakaFL <hei.sitraka@gmail.com>
2024-03-22 16:29:39 +0300

    feat: add annotation ComponentScan for the NexgenbankBackApplication class

--- src/main/java/prog4/hei/nexgenbank/nexgenbankback/NexgenbankBackApplication.java
+++ src/main/java/prog4/hei/nexgenbank/nexgenbankback/NexgenbankBackApplication.java
@@ -2,8 +2,10 @@ package prog4.hei.nexgenbank.nexgenbankback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "prog4.hei.nexgenbank.nexgenbankback")
 public class NexgenbankBackApplication {
 
     public static void main(String[] args) {
