846635538c11de567875372f1d3065fd70556e39
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-03-25 13:48:42 +0300

    fix: remove interest_duration_max column in MCD MPD

--- src/main/resources/db/migration/V5__0_create_insert_Loan_table.sql
+++ src/main/resources/db/migration/V5__0_create_insert_Loan_table.sql
@@ -4,15 +4,14 @@ CREATE TABLE IF NOT EXISTS "Loan" (
     amount DOUBLE PRECISION NOT NULL,
     interest_rate_for_the_first_7_days_of_overdraft INT NOT NULL,
     interest_rate_after_the_first_7_days_of_overdraft INT NOT NULL,
-    interest_pay_duration_max DATE NOT NULL,
     loan_status VARCHAR(250) DEFAULT 'Unpaid' CHECK (loan_status IN ('Unpaid','Paid')),
     id_Acount UUID REFERENCES "Account"(id)
 );
 
-INSERT INTO "Loan" (amount, interest_rate_for_the_first_7_days_of_overdraft, interest_rate_after_the_first_7_days_of_overdraft, interest_pay_duration_max,  id_Acount)
+INSERT INTO "Loan" (amount, interest_rate_for_the_first_7_days_of_overdraft, interest_rate_after_the_first_7_days_of_overdraft,loan_status,id_Acount)
 VALUES
-    (5000, 5, 10, '2025-03-16', 'Paid', (SELECT id FROM "Account" LIMIT 1)),
-    (6000, 6, 11, '2025-03-17','Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 1)),
-    (7000, 7, 12, '2025-03-18','Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 2)),
-    (8000, 8, 13, '2025-03-19','Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 3)),
-    (9000, 9, 14, '2025-03-20', 'Unpaid', (SELECT id FROM "Account" LIMIT 1 OFFSET 4));
+    (5000, 5, 10, 'Paid', (SELECT id FROM "Account" LIMIT 1)),
+    (6000, 6, 11, 'Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 1)),
+    (7000, 7, 12, 'Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 2)),
+    (8000, 8, 13, 'Paid', (SELECT id FROM "Account" LIMIT 1 OFFSET 3)),
+    (9000, 9, 14, 'Unpaid', (SELECT id FROM "Account" LIMIT 1 OFFSET 4));
